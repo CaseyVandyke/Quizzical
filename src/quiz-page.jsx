9eaa61e8b702cec8f9fd7e108242bb3b81d4c0d2
CaseyVandyke <caseyvandyke2@gmail.com>
2023-07-09 20:39:54 -0600

    added fetch to open trivia api and retrieved data

--- src/quiz-page.jsx
+++ src/quiz-page.jsx
@@ -1,7 +1,21 @@
+import {useState, useEffect} from 'react';
+
 const QuizPage = ({props}) => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('https://opentdb.com/api.php?amount=5&category=17&difficulty=medium&type=multiple')
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+
+    console.log(data);
+
     return (
-        <div>Quiz Page</div>
-    )
+        <div>
+            
+        </div>
+    );
 }
 
 export default QuizPage;
